63d089217efb26fb8066fad5062b0809caaffd55
Lherrera094 <herrera.woa94@gmail.com>
2025-05-09 08:32:58 -0600

    Update .sh file

--- FHELI.sh
+++ FHELI.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --nodes=1                   			# Use 1 node (OpenMP is shared-memory)
 #SBATCH --ntasks-per-node=1         			# Run 1 task (OpenMP threads handle parallelism)
-#SBATCH --cpus-per-task=96          			# Use all 96 logical cores (48 cores × 2 threads)
-#SBATCH --mem=370000M               			# ~370GB (leave 10GB for system overhead)
+#SBATCH --cpus-per-task=48          			# Max=96 logical cores (48 cores × 2 threads)
+#SBATCH --mem=40000M               			# ~370GB (leave 10GB for system overhead)
 #SBATCH --time=02:00:00             			# Max runtime 72:00:00 (3 days)
 #SBATCH --mail-type=BEGIN,END,FAIL 			# Replaces `-m bea` in msub or qsub
 #SBATCH --job-name=helicon_sim          		# Job name
 #SBATCH --output=omp_job_1.out     			# Output file (%j = job ID)
 #SBATCH --error=omp_job_1.err      			# Error file
-#SBATCH --partition=cpu  				# Replace with your cluster's partition name
+#SBATCH --partition=cpu  				# bwHPC=cpu_il, cpu, highmem, gpu_h100, gpu_mi300
 #SBATCH --mail-user=luis.herrera-quesada@igvp.uni-stuttgart.de
 
 EXECUTABLE=bin/./exe
